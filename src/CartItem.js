264c4de7e45daaa206257be5d99fb99aa6611897
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-07-09 09:33:37 +0530

    add state for cart vars

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,30 @@
 import React from 'react';
 class CartItem extends React.Component { // class based component
+    constructor (){
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
     render () {
+        const {title, price, qty} = this.state; // object destructuring so we do not have to write 'this.state' everytime
         return (
             <div className="cart-item">
                 <div className="left-blcok">
-                    <img style={styles.image    } />    
+                    <img style={styles.image} />    
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize: 25}}>Phone</div>
-                    <div style={{color: '#777'}}>Rs 999</div>
-                    <div style={{color: '#777'}}>Qty: 1</div>
+                    <div style={{fontSize: 25}}>{title}</div>
+                    <div style={{color: '#777'}}>Rs. {price}</div>
+                    <div style={{color: '#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992651.svg" />
+                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992683.svg" />
+                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1214/1214428.svg" />
                     </div>
                 </div>
             </div>    
